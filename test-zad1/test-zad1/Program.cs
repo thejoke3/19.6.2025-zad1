38d69da5ef7e27836f12b3134c37b78e766d6982
thejoke3 <167237678+thejoke3@users.noreply.github.com>
2025-06-19 10:57:07 +0300

    Add files via upload

---
+++ test-zad1/test-zad1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace test_zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string People = Console.ReadLine();
+           
+            bool StopNotFound = true;
+            Dictionary<string, int> pair = new Dictionary<string, int>();
+            while (StopNotFound)
+            {
+                if (People == "stop")
+                {
+
+                    break;
+                }
+
+                if (pair.ContainsKey(People))
+                {
+                    pair[People] += 1;
+                }
+                else
+                {
+                    pair.Add(People, 1);
+                }
+
+
+                People = Console.ReadLine();
+            }
+                
+            
+            foreach (var item in pair)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+        }
+    }
+}
